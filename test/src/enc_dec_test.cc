87f2d114cd0d1e397a7c61861e1e53f06f1b3b28
Arniiiii <lg3dx6fd@gmail.com>
2024-08-15 03:11:28 +0000

    fix test https://github.com/fmtlib/fmt/issues/4120

--- test/src/enc_dec_test.cc
+++ test/src/enc_dec_test.cc
@@ -43,7 +43,7 @@ struct fmt::formatter<MovableType> : formatter<int>
 {
   // parse is inherited from formatter<string_view>.
   template<typename FormatContext>
-  auto format(const MovableType& val, FormatContext& ctx) {
+  auto format(const MovableType& val, FormatContext& ctx) const {
     return formatter<int>::format(val.val[0].v, ctx);
   }
 };
